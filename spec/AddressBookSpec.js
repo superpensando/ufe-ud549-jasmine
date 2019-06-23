e5ec425bbae8a44dd4c34c411e97069b9f0d94e7
Laura Rodríguez Castillo <laura.rodriguez.castillo@gmail.com>
2019-06-23 12:33:41 +0200

    RedGreenRefactorCycle - Refactor the test (Adding deleteContact method)

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -2,7 +2,17 @@ describe("Address Book", function(){
     it("should be able to add a contact", function() {
         var addressBook = new AddressBook(); 
             thisContact = new Contact(); 
+
         addressBook.addContact(thisContact);
         expect(addressBook.getContact(0)).toBe(thisContact);
+    });
+    it("should be able to delete a contact", function(){
+        var addressBook = new AddressBook(); 
+            thisContact = new Contact(); 
+
+        addressBook.addContact(thisContact);
+        addressBook.deleteContact(0);
+        
+        expect(addressBook.getContact(0)).not.toBeDefined();
     });
 });
